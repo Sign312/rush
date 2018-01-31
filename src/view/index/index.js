9d22ebdfb0da5eb0e4a1db1a690fe1c026b4a8e8
小飞侠 <shixi_huyue3@staff.sina.com.cn>
2018-01-31 16:44:52 +0800

    fix(style): style

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,1 +1,8 @@
-console.log("🚲");
+import "./style.css";
+
+const gameCanvasEl = document.querySelector("#gameCanvas");
+
+gameCanvasEl.width = window.innerWidth;
+gameCanvasEl.height = window.innerHeight;
+
+const ctx = gameCanvasEl.getContext("2d");
---
+++ src/view/index/util/Animate.js
@@ -0,0 +1,7 @@
+class Animate {
+  constructor() {
+    
+  }
+}
+
+export default Animate;
